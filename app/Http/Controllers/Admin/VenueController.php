34b733b764f256b08ef836b0a83f196aa5963060
Kamer Durusoy <fkamerdurusoy@gmail.com>
2026-07-23 11:59:31 +0300

    requestler düzenlendi.

--- app/Http/Controllers/Admin/VenueController.php
+++ app/Http/Controllers/Admin/VenueController.php
@@ -14,12 +14,12 @@ class VenueController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Venues::class, 'venue');
+        $this->authorizeResource(Venue::class, 'venue');
     }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $search = $request->get('search');
         
@@ -39,7 +39,7 @@ class VenueController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreVenueRequest $request)
     {
         $data = $request->validated();
 
@@ -73,7 +73,7 @@ class VenueController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateVenueRequest $request, string $id)
     {
         $data = $request->validated();
 
