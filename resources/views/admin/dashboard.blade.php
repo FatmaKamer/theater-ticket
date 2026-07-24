92341c316dc37dd0fa7709d2c5eb8436dac506a4
Kamer Durusoy <fkamerdurusoy@gmail.com>
2026-07-24 14:22:52 +0300

    Admin paneline oyunlar kartı eklendi.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <span>Admin</span> Paneli
             </h1>
             <p class="theater-subtitle">Hoş geldiniz, {{ auth()->user()->name }}!</p>
-            
+
             <div class="row mt-4">
                 <div class="col-md-4">
                     <div class="card">
@@ -30,6 +30,19 @@
                             <h5 class="card-title mt-3">Salonlar</h5>
                             <p class="card-text">Toplam {{ \App\Models\Venue::count() }} salon</p>
                             <a href="{{ route('admin.venues.index') }}" class="btn-theater">
+                                <i class="fas fa-arrow-right"></i> Yönet
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <i class="fas fa-theater-masks" style="font-size: 48px; color: #800020;"></i>
+                            <h5 class="card-title mt-3">Oyunlar</h5>
+                            <p class="card-text">Toplam {{ \App\Models\Play::count() }} oyun</p>
+                            <a href="{{ route('admin.plays.index') }}" class="btn-theater">
                                 <i class="fas fa-arrow-right"></i> Yönet
                             </a>
                         </div>
