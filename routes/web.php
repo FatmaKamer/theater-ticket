7a908b0648c0ab92453f2d25a30017a90a3ad756
Kamer Durusoy <fkamerdurusoy@gmail.com>
2026-07-29 11:19:59 +0300

    bilet yönetim sisteminin rotaları tanımlandı.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\PlayController;
+use App\Http\Controllers\Admin\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\UserController;
@@ -30,4 +31,5 @@ Route::middleware(['auth','admin'])->prefix('admin')->name('admin.')->group(func
 
     Route::resource('plays', PlayController::class);
 
+    Route::resource('tickets', TicketController::class)->only(['index', 'show', 'destroy']);
 });
